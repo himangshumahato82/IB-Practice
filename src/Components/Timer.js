91b34f756f4d600fdc4de960d05de8ffae4e975f
himangshumahato82 <himangshumahato82@gmail.com>
2023-03-18 22:20:23 +0530

    update

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -2,28 +2,14 @@ import React from 'react';
 import { useState,useRef } from 'react';
 function Timer(props) {
 
-    const [count, setCount] = useState(0);
-    const timer = useRef();
-
-    const start = () => {
-      timer.current = setInterval(() => {
-        setCount((pre) => pre + 1);
-      }, 1000);
-    };
-    const stop = () => {
-      clearInterval(timer.current);
-    };
-    const reset = () => {
-      stop();
-      setCount(0);
-    };
+    
   
     return (
       <div>
-        <h1>{count}</h1>
-        <button onClick={start}>Start</button>
-        <button onClick={stop}>Stop</button>
-        <button onClick={reset}>Reset</button>
+        <h1></h1>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Pause</button>
       </div>
     );
 }
