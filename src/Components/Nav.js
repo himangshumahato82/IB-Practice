515ac4db7748ea2a7ebf70346d6a4e038333b5ed
himangshumahato82 <himangshumahato82@gmail.com>
2023-04-30 04:40:50 +0530

    date

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,13 +2,45 @@ import React from 'react';
 import { useEffect,useState} from 'react';
 import { Link } from 'react-router-dom';
 function Nav(props) {
-
+    useEffect(()=>{
+        SmartWatch()
+    },[])
+      const[smart,setSmart]=useState([])
+        const SmartWatch=()=>{
+      setInterval(()=>{
+    
+     
+       const date=new Date()
+       
+       let hr=date.getHours()
+      let min=date.getMinutes() 
+      let sec=date.getSeconds()
+       
+      hr=hr.toString().padStart(2,"0")
+      min=min.toString().padStart(2,"0")
+      sec=sec.toString().padStart(2,"0")
+     const arr=[hr,":",min,":",sec]
+     setSmart(arr)
+    // console.log(arr)
+    //  console.log(smart)
+    // console.log(hr,min,sec)
+    // const timeString = `${hr}:${min}:${sec}`;
+    // hour.textContent = hr;
+    //    minute.textContent = min;
+    //    seconds.textContent = sec;
+        
+    },1000)
+    // const hour = document.getElementById("hour");
+    // const minute = document.getElementById("minute");
+    // const seconds = document.getElementById("seconds");
+        }
 
 
     return (
         <div style={{display:"flex",justifyContent:"space-evenly",height:"60px",backgroundColor:"teal",alignItems:"center"}}>
-            
+        <h1 style={{color:"hsl(75, 84%, 47%)"}}> {smart}</h1>
               <Link to ={"/"}><h1>Login</h1></Link>
+               
               <Link to ={"/timer"}>Timmer</Link>
              <Link to={"/home"}><h1>Home</h1></Link>
              <Link to={"/useref"}>UseRef</Link>
